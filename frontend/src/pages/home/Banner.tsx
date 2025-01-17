134624461a871ac8dbcbaad5af1ad8710d9db1ba
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-01-17 16:19:29 -0400

    add hero banner

---
+++ frontend/src/pages/home/Banner.tsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+
+import bannerImg from '../../assets/header.png';
+
+const Banner = () => {
+  return (
+    <div className="section__container header__container">
+      <div className="header__content z-30">
+        <h4 className="uppercase">UP TO 25% DISCOUNT ON</h4>
+        <h1>Girl's fashion</h1>
+        <p>
+          Browse and discover incredible offers on clothing and accesories for
+          women.
+        </p>
+        <button className="btn">
+          <Link to="/shop">EXPLORE NOW</Link>
+        </button>
+      </div>
+      <div className="header__image">
+        <img src={bannerImg} alt="banner image" />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
