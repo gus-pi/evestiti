ba1b8fc7ae88bcea40c148944acaa6ed49567c08
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-01-18 09:02:55 -0400

    add come categories component

---
+++ frontend/src/pages/home/Categories.tsx
@@ -0,0 +1,47 @@
+import { Link } from 'react-router-dom';
+import category1 from '../../assets/category-1.jpg';
+import category2 from '../../assets/category-2.jpg';
+import category3 from '../../assets/category-3.jpg';
+import category4 from '../../assets/category-4.jpg';
+
+const Categories = () => {
+  const categories = [
+    {
+      name: 'Accesories',
+      path: 'accesories',
+      image: category1,
+    },
+    {
+      name: 'Dress Collection',
+      path: 'dress',
+      image: category2,
+    },
+    {
+      name: 'Jewelry',
+      path: 'jewelry',
+      image: category3,
+    },
+    {
+      name: 'Cosmetics',
+      path: 'cosmetics',
+      image: category4,
+    },
+  ];
+  return (
+    <>
+      <div className="product__grid">
+        {categories.map((category) => (
+          <Link
+            to={`/categories/${category.path}`}
+            className="categories__card"
+          >
+            <img src={category.image} alt={category.name} />
+            <h4>{category.name}</h4>
+          </Link>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Categories;
