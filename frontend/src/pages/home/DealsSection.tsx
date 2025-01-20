48703a917df323129766dac1611bd614a74c3e16
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-01-20 11:09:39 -0400

    add deals of the month section

---
+++ frontend/src/pages/home/DealsSection.tsx
@@ -0,0 +1,31 @@
+import dealsImg from '../../assets/deals.png';
+
+const DealsSection = () => {
+  return (
+    <section className="section__container deals__container">
+      <div className="deals__image">
+        <img src={dealsImg} alt="deal" />
+      </div>
+      <div className="deals__content">
+        <h5>Get Up To 20% Discounts</h5>
+        <h4>Deals Of The Month</h4>
+        <p>
+          Deals for the month of January, make sure to take advantage of this
+          amazing deals on clothing, accesories, footwear and many more!
+        </p>
+        <div className="deals__countdown flex-wrap">
+          <div className="deals__countdown__card">
+            <h4>14</h4>
+            <p>Days</p>
+          </div>
+          <div className="deals__countdown__card">
+            <h4>20</h4>
+            <p>Hours</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default DealsSection;
