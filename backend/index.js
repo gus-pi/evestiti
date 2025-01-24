828e16fa9fdc9b6f21dfaf641f066149246d8b65
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-01-24 13:15:56 -0400

    install mongodb and mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Evestiti Server is Running!');
-});
+main()
+  .then(() => console.log('mongodb successfully connected'))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(
+    'mongodb+srv://admin:admin@evestiti.j5gav.mongodb.net/?retryWrites=true&w=majority&appName=evestiti'
+  );
+
+  app.get('/', (req, res) => {
+    res.send('Evestiti Server is Running!');
+  });
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
