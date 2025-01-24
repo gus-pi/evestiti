b0a7fc0cb1fa7b26d21632db2853615b579fbf68
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-01-24 13:50:17 -0400

    add dotenv, cros, cookie and body parser

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+//middlewares setup
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ limit: '25mb' }));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);
 
 main()
   .then(() => console.log('mongodb successfully connected'))
   .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect(
-    'mongodb+srv://admin:admin@evestiti.j5gav.mongodb.net/?retryWrites=true&w=majority&appName=evestiti'
-  );
+  await mongoose.connect(process.env.DB_URL);
 
   app.get('/', (req, res) => {
     res.send('Evestiti Server is Running!');
