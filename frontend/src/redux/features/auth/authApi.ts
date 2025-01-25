0da196ca4d6eb2a349e1d88fc1095309c45f13d7
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-01-25 12:22:01 -0400

    add logout, delete, update to auth api

--- frontend/src/redux/features/auth/authApi.ts
+++ frontend/src/redux/features/auth/authApi.ts
@@ -7,6 +7,7 @@ const authApi = createApi({
         baseUrl: `${getBaseUrl()}/api/auth`,
         credentials: 'include'
     }),
+    tagTypes: ['User'],
     endpoints: (builder) => ({
         registerUser: builder.mutation({
             query: (newUser) => ({
@@ -22,9 +23,45 @@ const authApi = createApi({
                 body: credentials
             })
         }),
+        logoutUser: builder.mutation({
+            query: () => ({
+                url: '/logout',
+                method: 'DELETE'
+            })
+        }),
+        getUsers: builder.mutation({
+            query: () => ({
+                url: '/users',
+                method: 'GET'
+            }),
+            invalidatesTags: ["User"],
+        }),
+        deleteUser: builder.mutation({
+            query: (userId) => ({
+                url: `/users/${userId}`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: ["User"],
+        }),
+        updateUserRole: builder.mutation({
+            query: ({ userId, role }) => ({
+                url: `/users/${userId}`,
+                method: 'PUT',
+                body: { role }
+            }),
+            invalidatesTags: ["User"],
+        }),
+        editProfile: builder.mutation({
+            query: (profileData) => ({
+                url: 'edit-profile',
+                method: 'PATCH',
+                body: profileData
+            }),
+            invalidatesTags: ["User"],
+        }),
     })
 
 })
 
-export const { useRegisterUserMutation, useLoginUserMutation } = authApi
+export const { useRegisterUserMutation, useLoginUserMutation, useLogoutUserMutation, useGetUsersMutation, useUpdateUserRoleMutation, useDeleteUserMutation, useEditProfileMutation } = authApi
 export default authApi
