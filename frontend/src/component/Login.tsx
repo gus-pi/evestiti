896f3a421494b321412110100750322023ccc31f
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-01-25 11:29:00 -0400

    create authSlice for logged in user

--- frontend/src/component/Login.tsx
+++ frontend/src/component/Login.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { useLoginUserMutation } from '../redux/features/auth/authApi';
+import { setUser } from '../redux/features/auth/authSlice';
 
 const Login = () => {
   const [message, setMessage] = useState('');
@@ -22,6 +23,11 @@ const Login = () => {
 
     try {
       const response = await loginUser(data).unwrap();
+
+      const { token, user } = response;
+
+      dispatch(setUser(user));
+
       alert('Login succesful');
       navigate('/');
     } catch (error) {
--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import cartReducer from './features/cart/cartSlice'
+import authReducer from './features/auth/authSlice'
 import authApi from './features/auth/authApi'
 
 export const store = configureStore({
     reducer: {
         cart: cartReducer,
-        [authApi.reducerPath]: authApi.reducer
+        [authApi.reducerPath]: authApi.reducer,
+        auth: authReducer
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(authApi.middleware),
