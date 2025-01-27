1379e9536fb4bba701dc4cd2fd0e11d41c2e867e
PieMenard <pinedo.m.gustavo@gmail.com>
2025-01-26 23:54:02 -0400

    use api in single product page

--- frontend/src/pages/shop/productDetails/SingleProduct.tsx
+++ frontend/src/pages/shop/productDetails/SingleProduct.tsx
@@ -1,8 +1,26 @@
 import { Link, useParams } from 'react-router-dom';
 import RatingStars from '../../../component/RatingStars';
+import { useDispatch } from 'react-redux';
+import { useFetchProductByIdQuery } from '../../../redux/features/products/productApi';
+import { addToCart } from '../../../redux/features/cart/cartSlice';
+import { Product } from '../../../types/types';
 
 const SingleProduct = () => {
   const { id } = useParams();
+
+  const dispatch = useDispatch();
+  const { data, error, isLoading } = useFetchProductByIdQuery(id);
+
+  const singleProduct = data?.product || {};
+  const productReviews = data?.reviews || [];
+
+  const handleAddToCart = (product: Product) => {
+    dispatch(addToCart(product));
+  };
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error loading product</p>;
+
   return (
     <>
       <section className="section__container bg-primary-light">
@@ -16,7 +34,7 @@ const SingleProduct = () => {
             <Link to="/shop">shop</Link>
           </span>
           <i className="ri-arrow-right-s-line"></i>
-          <span className="hover:text-primary">product name</span>
+          <span className="hover:text-primary">{singleProduct.name}</span>
         </div>
       </section>
 
@@ -24,35 +42,41 @@ const SingleProduct = () => {
         <div className="flex flex-col items-center md:flex-row gap-8">
           <div className="md:w-1/2 w-full">
             <img
-              src="https://images.unsplash.com/photo-1568251188392-ae32f898cb3b?q=80&w=2062&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              src={singleProduct?.image}
               alt=""
               className="rounded-md w-full h-auto"
             />
           </div>
 
           <div className="md:w-1/2 w-full">
-            <h3 className="text-2xl font-semibold mb-4">Product Name</h3>
-            <p className="text-xl text-primary mb-4">
-              $100 <s>$130</s>
+            <h3 className="text-2xl font-semibold mb-4">
+              {singleProduct?.name}
+            </h3>
+            <p className="text-xl text-primary mb-4 space-x-1">
+              ${singleProduct?.price}
+              {singleProduct.oldPrice && (
+                <s className="ml-1">${singleProduct?.oldPrice}</s>
+              )}
             </p>
-            <p className="text-gray-700 mb-4">
-              This is the product description
-            </p>
+            <p className="text-gray-700 mb-4">{singleProduct?.description}</p>
 
-            <div>
-              <p>
-                <strong>Category:</strong> accessories
+            <div className="flex flex-col space-y-2">
+              <p className="capitalize">
+                <strong>Category:</strong> {singleProduct?.category}
               </p>
-              <p>
-                <strong>Color:</strong> beige
+              <p className="capitalize">
+                <strong>Color:</strong> {singleProduct?.color}
               </p>
               <div className="flex gap-1 items-center">
                 <strong>Rating: </strong>
-                <RatingStars rating={4} />
+                <RatingStars rating={singleProduct?.rating} />
               </div>
             </div>
 
-            <button className="mt-6 px-6 py-3 bg-primary text-white rounded-md">
+            <button
+              onClick={() => handleAddToCart(singleProduct)}
+              className="mt-6 px-6 py-3 bg-primary text-white rounded-md"
+            >
               Add to Cart
             </button>
           </div>
