9c765cce2e585a5bbb985e53f26c6738144df866
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-01-27 16:28:30 -0400

    implement post a new review, start

--- frontend/src/pages/shop/reviews/PostAReview.tsx
+++ frontend/src/pages/shop/reviews/PostAReview.tsx
@@ -1,10 +1,68 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { RootState } from '../../../redux/store';
+import { useFetchProductByIdQuery } from '../../../redux/features/products/productApi';
+import { usePostReviewMutation } from '../../../redux/features/reviews/reviewsApi';
+
 type PostAReviewProps = {
   isModalOpen: boolean;
   handleClose: () => void;
 };
 
 const PostAReview = ({ isModalOpen, handleClose }: PostAReviewProps) => {
-  return <div>Post a review {isModalOpen}</div>;
+  const { id } = useParams();
+  const { user } = useSelector((state: RootState) => state.auth);
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState('');
+
+  const { refetch } = useFetchProductByIdQuery(id, { skip: !id });
+  const [postReview] = usePostReviewMutation();
+
+  const handleRating = (value: number) => {
+    setRating(value);
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const newComment = {
+      comment,
+      rating,
+      userId: user?._id,
+      product: id,
+    };
+
+    try {
+    } catch (error) {}
+  };
+
+  return (
+    <div
+      className={`fixed inset-0 bg-black/90 flex items-center justify-center z-40 px-2 ${
+        isModalOpen ? 'block' : 'hidden'
+      }`}
+    >
+      <div className="bg-white p-6 rounded-md shadow-lg w-96 z-50">
+        <h2 className="text-lg font-medium mb-4">Post A Review</h2>
+
+        <div className="flex items-center mb-4">
+          {[1, 2, 3, 4, 5].map((star) => (
+            <span
+              key={star}
+              onClick={() => handleRating(star)}
+              className="cursor-pointer text-yellow-500 text-lg"
+            >
+              {rating >= star ? (
+                <i className="ri-star-fill"></i>
+              ) : (
+                <i className="ri-star-fill"></i>
+              )}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PostAReview;
