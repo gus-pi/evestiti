5ab40890d28e98eebf0302d2822b784578dde8ef
PieMenard <pinedo.m.gustavo@gmail.com>
2025-01-26 20:02:32 -0400

    create product api using rtk query

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,20 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
 
-import cartReducer from './features/cart/cartSlice'
-import authReducer from './features/auth/authSlice'
-import authApi from './features/auth/authApi'
+import cartReducer from './features/cart/cartSlice';
+import authReducer from './features/auth/authSlice';
+import authApi from './features/auth/authApi';
+import productsApi from './features/products/productApi';
 
 export const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-        [authApi.reducerPath]: authApi.reducer,
-        auth: authReducer
-    },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(authApi.middleware),
-})
+  reducer: {
+    cart: cartReducer,
+    [authApi.reducerPath]: authApi.reducer,
+    auth: authReducer,
+    [productsApi.reducerPath]: productsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware, productsApi.middleware),
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
