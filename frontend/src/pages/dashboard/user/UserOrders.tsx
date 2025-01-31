ffc3e6750028c0aef4a3e752526d10aeedca1fd3
PieMenard <pinedo.m.gustavo@gmail.com>
2025-01-31 00:03:14 -0400

    add dollar sign to total

--- frontend/src/pages/dashboard/user/UserOrders.tsx
+++ frontend/src/pages/dashboard/user/UserOrders.tsx
@@ -97,7 +97,7 @@ const UserOrders = () => {
                         </span>
                       </td>
                       <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                        {order.amount}
+                        ${order.amount}
                       </td>
                       <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                         <Link
